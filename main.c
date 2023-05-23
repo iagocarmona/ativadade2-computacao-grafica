2264d8118ece8e553313087ba1da94c3d0a5cd2f
Iago Carmona <69121686+iagocarmona@users.noreply.github.com>
2023-05-23 15:28:17 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 float alpha=0, beta=0, delta=1;
 
 int init(void){
-    glClearColor(0.0, 0.0, 0.0, 0.0);     //define a cor de fundo
+    glClearColor(1.0, 1.0, 1.0, 1.0);     //define a cor de fundo
     glEnable(GL_DEPTH_TEST);           //remo��o de superf�cie oculta
 
     glMatrixMode(GL_PROJECTION);          //define que a matriz � a de proje��o
     glLoadIdentity();                     //carrega a matriz de identidade
-    glOrtho(-5,5,-5,5,-5,5);          //define uma proje��o ortogr�fica
+    glOrtho(-3,3,-3,3,-1,50);          //define uma proje��o ortogr�fica
 }
 
 void display(void){
@@ -22,12 +22,12 @@ void display(void){
 
     glScalef(delta, delta, delta); //fa�a a escala de todos objetos
 
-    glColor3f(1, 1, 0);
+    glColor3f(1, 0, 0);
     glPushMatrix(); //armazena a matriz corrente
         glTranslatef(-3, 0, 0);
         glRotatef(beta, 0, 1, 0);
         glRotatef(alpha, 1, 0, 0);
-        glutWireSphere(1, 20, 20);
+        glutWireTeapot(1);
     glPopMatrix(); //restaura a matriz anterior
 
     glColor3f(1, 0, 0);
@@ -35,7 +35,23 @@ void display(void){
         glTranslatef(3, 0, 0);
         glRotatef(beta, 0, 1, 0);
         glRotatef(alpha, 1, 0, 0);
-        glutWireSphere(1, 20, 20);
+        glutWireTeapot(1);
+    glPopMatrix(); //restaura a matriz anterior
+
+    glColor3f(1, 0, 0);
+    glPushMatrix(); //armazena a matriz corrente
+        glTranslatef(-3, 2, 0);
+        glRotatef(beta, 0, 1, 0);
+        glRotatef(alpha, 1, 0, 0);
+        glutWireTeapot(1);
+    glPopMatrix(); //restaura a matriz anterior
+
+    glColor3f(1, 0, 0);
+    glPushMatrix(); //armazena a matriz corrente
+        glTranslatef(3, 3, 0);
+        glRotatef(beta, 0, 1, 0);
+        glRotatef(alpha, 1, 0, 0);
+        glutWireTeapot(1);
     glPopMatrix(); //restaura a matriz anterior
 
     //for�a o desenho das primitivas
@@ -46,7 +62,7 @@ int main(int argc, char** argv){
     glutInit(&argc,argv);                           //inicializa o GLUT
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);    //configura o modo de display
     glutInitWindowPosition(200,0);                  //seta a posi��o inicial da janela
-    glutInitWindowSize(400,400);                    //configura a largura e altura da janela de exibi��o
+    glutInitWindowSize(800,800);                    //configura a largura e altura da janela de exibi��o
     glutCreateWindow("3D");                 //cria a janela de exibi��o
 
     init();                                         //executa fun��o de inicializa��o
