d45ab4816d3b805e5690714060a9d0e50e27be1b
iagocarmona <iagoortegacarmona@gmail.com>
2023-05-25 17:29:30 -0300

    Adicionando timer

--- main2.c
+++ main2.c
@@ -1,43 +1,67 @@
 #include <GL/freeglut.h>
 
+
+float dia = 0, hora = 0;
+
+void Timer(int value){
+    hora += 1.0f;
+    if (hora >= 24.0f){
+        hora = 0.0f;
+        dia += 1.0f;
+        if (dia >= 360.0f){
+            dia = 0.0f;
+        }
+    }
+    glutPostRedisplay();
+    glutTimerFunc(40,Timer, 1);
+}
+
 void init() {
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glEnable(GL_DEPTH_TEST);
 
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
 }
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
     // Viewport 1 (top-left)
-    glViewport(0, 400, 400, 400);
-    glMatrixMode(GL_MODELVIEW);
+    glViewport(0, 300, 300, 300);
+    glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(-3, 3, -3, 3, 1, 50);
-    gluLookAt(0, 0, 10,
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0, 30, 0,
               0.0, 0.0, 0.0,
-              0.0, 1.0, 0.0);
+              0.0, 0.0, -1.0);
     glColor3f(1, 0, 0);
     glutWireTeapot(1.5);
 
     // Viewport 2 (top-right)
-    glViewport(400, 400, 400, 400);
-    glMatrixMode(GL_MODELVIEW);
+    glViewport(300, 300, 300, 300);
+    glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(-3, 3, -3, 3, 1, 50);
-    gluLookAt(0, 0, 10,
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(10, 0, 0,
               0.0, 0.0, 0.0,
               0.0, 1.0, 0.0);
     glColor3f(1, 0, 0);
     glutWireTeapot(1.5);
 
     // Viewport 3 (bottom-left)
-    glViewport(0, 0, 400, 400);
-    glMatrixMode(GL_MODELVIEW);
+    glViewport(0, 0, 300, 300);
+    glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(-3, 3, -3, 3, 1, 50);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
     gluLookAt(0, 0, 10,
               0.0, 0.0, 0.0,
               0.0, 1.0, 0.0);
@@ -45,10 +69,13 @@ void display() {
     glutWireTeapot(1.5);
 
     // Viewport 4 (bottom-right)
-    glViewport(400, 0, 400, 400);
-    glMatrixMode(GL_MODELVIEW);
+    glViewport(300, 0, 300, 300);
+    glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(-3, 3, -3, 3, 1, 50);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
     gluLookAt(0, 0, 10,
               0.0, 0.0, 0.0,
               0.0, 1.0, 0.0);
@@ -62,11 +89,12 @@ int main(int argc, char** argv){
     glutInit(&argc,argv);                           //inicializa o GLUT
     glutInitDisplayMode(GLUT_SINGLE| GLUT_RGB);    //configura o modo de display
     glutInitWindowPosition(200,0);                  //seta a posi��o inicial da janela
-    glutInitWindowSize(800,800);                    //configura a largura e altura da janela de exibi��o
+    glutInitWindowSize(600,600);                    //configura a largura e altura da janela de exibi��o
     glutCreateWindow("3D");                 //cria a janela de exibi��o
 
     init();                                         //executa fun��o de inicializa��o
     glutDisplayFunc(display);
+    glutTimerFunc(40,Timer, 1);
     glutMainLoop();                                  //mostre tudo e espere
     return 0;
 
